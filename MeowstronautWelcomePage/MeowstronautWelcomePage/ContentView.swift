8139ef74cafdc41fa3b6a90368d699f8ee1152d8
HaoyangGuo1116 <haoyang1116@outlook.com>
2025-09-16 17:47:13 -0700

    navigation pages

--- MeowstronautWelcomePage/MeowstronautWelcomePage/ContentView.swift
+++ MeowstronautWelcomePage/MeowstronautWelcomePage/ContentView.swift
@@ -9,14 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        MainPage()
+    }
+}
+
+
+
+
+struct MainPage: View {
+    
+    @State var showingSheet = false
+    
+    var body: some View {
         ZStack(alignment: .top) {
-            Image("SpacePic")
+            Image("Space2D")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea(edges: .all)
             
             VStack {
-                Text("Hello  World!")
+                Text("Meowstroaut")
                     .font(.system(size: 48, weight: .heavy, design: .rounded))
                     .foregroundColor(.white)
                     .shadow(color: .blue, radius: 2)
@@ -37,6 +49,15 @@ struct ContentView: View {
                     .tracking(3)
                     .shadow(color: .white.opacity(0.7), radius: 4, x:0, y:-10)
                     
+                    Button("Shop") {
+                        print("Shop tapped")
+                    }
+                    .font(.system(size: 22, weight: .bold, design: .rounded))
+                    .foregroundColor(.white)
+                    .tracking(3)
+                    .shadow(color: .white.opacity(0.7), radius: 4, x:0, y:-10)
+                    
+                    
                     Button("Scoreboard") {
                         print("Scoreboard tapped")
                     }
@@ -53,31 +74,80 @@ struct ContentView: View {
                     .tracking(3)
                     .shadow(color: .white.opacity(0.7), radius: 4, x:0, y:-10)
                     
-                    Button("Credits") {
-                        print("Credits tapped")
+                    Button(action: {showingSheet.toggle()}) {
+                        Text("Credits")
+                        //print("Credits tapped")
                     }
                     .font(.system(size: 22, weight: .bold, design: .rounded))
                     .foregroundColor(.white)
                     .tracking(3)
                     .shadow(color: .white.opacity(0.7), radius: 4, x:0, y:-10)
+                    .sheet(isPresented: $showingSheet,
+                           onDismiss: didDismiss){
+                        ZStack(alignment: .top) {
+                            Image("Space2D")
+                                .resizable()
+                                .scaledToFill()
+                                .ignoresSafeArea(edges: .all)
+                            
+                            VStack(spacing: 70) {
+                                Text("Credits")
+                                    .font(.system(size: 48, weight: .heavy, design: .rounded))
+                                    .foregroundColor(.white)
+                                    .shadow(color: .blue, radius: 2)
+                                    .shadow(color: .white.opacity(0.5), radius: 6, x:0, y:-15)
+                                    .rotation3DEffect(.degrees(20), axis: (x: 0.2, y: 0.0, z: 0.0))
+                                    .tracking(4)
+                                    //.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                                    .padding(.top, 70)
+                                
+                                //Spacer()
+                                
+                                Text("Developed by:")
+                                    .font(.system(size: 22, weight: .bold, design: .rounded))
+                                    .foregroundColor(.white)
+                                    .tracking(3)
+                                    .shadow(color: .white.opacity(0.7), radius: 4, x:0, y:-10)
+                                
+                                Text("Haoyang Guo")
+                                    .font(.system(size: 22, weight: .bold, design: .rounded))
+                                    .foregroundColor(.white)
+                                    .tracking(3)
+                                    .shadow(color: .white.opacity(0.7), radius: 4, x:0, y:-10)
+                                Text("XXX")
+                                    .font(.system(size: 22, weight: .bold, design: .rounded))
+                                    .foregroundColor(.white)
+                                    .tracking(3)
+                                    .shadow(color: .white.opacity(0.7), radius: 4, x:0, y:-10)
+                                Text("XXX")
+                                    .font(.system(size: 22, weight: .bold, design: .rounded))
+                                    .foregroundColor(.white)
+                                    .tracking(3)
+                                    .shadow(color: .white.opacity(0.7), radius: 4, x:0, y:-10)
+                            }
+                            
+                        }
+                    }
+                    
                 }
-                .padding(.bottom, 150)
                 
+                
+                Button(action: {}) {
+                    Image(systemName: "speaker.wave.2")
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                        .padding(.trailing, 100)
+                        .padding(.bottom, 40)
                 }
-        
-            
-        Button(action: {}) {
-            Image(systemName: "speaker.wave.2")
-                .font(.system(size: 20, weight: .bold, design: .rounded))
-                .foregroundColor(.white)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
-                .padding(.trailing, 170)
-                .padding(.bottom, 40)
             }
         }
     }
+    func didDismiss() {
+            print("Dismissed")
+        }
 }
-
-#Preview {
-    ContentView()
-}
+    
+    #Preview {
+        ContentView()
+    }
